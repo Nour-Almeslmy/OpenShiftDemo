083e179c927ab749c7fcf56cfeeb7e3e00dfa8a7
Nour Almelsmy <91690116+Nour-Almeslmy@users.noreply.github.com>
2023-03-14 13:36:04 +0200

    add logger controller

---
+++ OpenShiftDemo_Business/Controllers/LoggerController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OpenShiftDemo_Business.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class LoggerController : ControllerBase
+    {
+        private readonly ILogger<LoggerController> _logger;
+
+        public LoggerController(ILogger<LoggerController> logger)
+        {
+            _logger = logger;
+        }
+
+        public string Log()
+        {
+            try
+            {
+                string? error = null;
+                error.ToUpper();
+            }
+            catch (Exception exp)
+            {
+                _logger.LogDebug("logging EndPoint Debug");
+                _logger.LogError(exp, "Logging Endpoint Error", new object());
+            }
+            return $"Logging";
+        }
+    }
+}
