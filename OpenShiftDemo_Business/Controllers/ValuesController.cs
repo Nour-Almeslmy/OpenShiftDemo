2f95ef28ce5d7235db46162acee009e35ec732d8
Nour Almelsmy <91690116+Nour-Almeslmy@users.noreply.github.com>
2023-03-14 11:48:51 +0200

    add values controller

---
+++ OpenShiftDemo_Business/Controllers/ValuesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OpenShiftDemo_Utilities;
+
+namespace OpenShiftDemo_Business.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public int Get()
+        {
+            var val = new Values();
+            return val.GetRand(1, 10);
+        }
+    }
+}
