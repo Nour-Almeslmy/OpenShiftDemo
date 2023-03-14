536b295477dfd2892c72c94f40e6be0386550397
Nour Almelsmy <91690116+Nour-Almeslmy@users.noreply.github.com>
2023-03-14 11:37:36 +0200

    add class library utils

---
+++ OpenShiftDemo_Utilities/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace OpenShiftDemo_Utilities
+{
+    public class Values
+    {
+        public int GetRand(int start, int end)
+        {
+            Random rnd = new Random();
+            return rnd.Next(start, end);
+        }
+    }
+}
